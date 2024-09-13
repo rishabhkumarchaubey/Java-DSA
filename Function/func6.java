ed62594237ea41990361d6adc10faa2683aec2e9
RISHABH KUMAR <chaubeyr1234@gmail.com>
2024-09-13 11:38:11 +0530

    function

--- Function/func6.java
+++ Function/func6.java
@@ -1,5 +1,13 @@
 public class func6 {
+    public static int sum(int a, int b){
+        return a+b;
+    }
+    public static int sum(int a, int b, int c){
+        return a+b+c;
+    }
     public static void main(String[] args) {
+        System.out.println(sum(4, 4));
+        System.out.println(sum(2, 3, 2));
         
     }
     
